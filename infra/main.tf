6c53a7a7ca731cb7302cff608cc77fecd2d67f2e
Gabriel Benavides <grilles_telling.7v@icloud.com>
2026-02-09 13:18:43 -0500

    add bucket

--- infra/main.tf
+++ infra/main.tf
@@ -2,6 +2,11 @@ terraform {
   required_providers {
     aws = { source = "hashicorp/aws", version = "~> 5.0" }
   }
+   backend "s3" {
+    bucket = "tf-state-demo-platform"
+    key    = "services/demo-01.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
