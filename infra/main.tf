1d8b5121321cd4eab42e1209548973dd5d2ad740
Gabriel Benavides <grilles_telling.7v@icloud.com>
2026-02-10 23:43:33 -0500

    update

--- infra/main.tf
+++ infra/main.tf
@@ -19,10 +19,6 @@ locals {
   # - minúsculas
   # - ':' -> '-'
   ecr_repo_name = lower(replace(var.cod_servicio, ":", "-"))
-
-  # App Runner solo si se pidió y hay imagen inicial
-  #do_apprunner = var.crear_apprunner && length(trimspace(var.imagen_inicial)) > 0
-  do_apprunner = var.crear_apprunner
 }
 
 resource "aws_ecr_repository" "svc" {
@@ -30,24 +26,3 @@ resource "aws_ecr_repository" "svc" {
   force_delete = true
 }
 
-resource "aws_apprunner_service" "svc" {
-  count = local.do_apprunner ? 1 : 0
-
-  service_name = "${local.ecr_repo_name}-service"
-
-  source_configuration {
-    authentication_configuration {
-      access_role_arn = var.apprunner_access_role_arn
-    }
-    image_repository {
-      image_identifier      = "${aws_ecr_repository.svc.repository_url}:${var.imagen_inicial}"
-      image_repository_type = "ECR"
-
-      image_configuration {
-        port = "8080"
-      }
-    }
-
-    auto_deployments_enabled = false
-  }
-}
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,19 +1,5 @@
 variable "cod_servicio" { type = string }
 variable "region" { type = string }
 
-variable "crear_apprunner" {
-  type    = bool
-  default = true
-}
-
-variable "imagen_inicial" {
-  type    = string
-  default = "bootstrap"
-}
-
-
-variable "apprunner_access_role_arn" {
-  type    = string
-}
 
 
